c55983b5aee1504430842aee430336c84a5d6401
maisarahmusa <maisarah.musa@pipeline-network.com>
2023-03-03 09:06:34 +0800

    Javascript

---
+++ maisarah/Exercise29/app.js
@@ -0,0 +1,14 @@
+function taknampak(){
+    let tknmpak = window.confirm("Do you want to continue?");
+
+    if (tknmpak){
+        alert('OK');
+    }
+    else
+        alert('Cancel');
+}
+
+function taknampak2(){
+    let tknmpak2 = prompt("Enter your name");
+    alert(`How are you doing?, ${tknmpak2}`);
+}
